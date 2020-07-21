0cf11e85c76dbd998f7054190dba7ef3a8566071
Jasper Reed <jmixon.reed@gmail.com>
2020-07-21 01:49:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,7 +66,7 @@ def game_hash
     },
     :away => {
       :team_name = "Charlotte Hornets",
-      :colors = [""],
+      :colors = ["Turquoise", "Purple"],
       :players = [
         {
           :player_name =>,
