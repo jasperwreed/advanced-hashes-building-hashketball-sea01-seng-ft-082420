1e9a00e6095199f1c537eaff9acdafbd191aa5de
Jasper Reed <jmixon.reed@gmail.com>
2020-07-21 02:15:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 # Write your code here!
 def game_hash
-  hasketball = {
     :home => {
       :team_name = "Brooklyn Nets",
       :colors = ["Black", "White"],
@@ -125,5 +124,4 @@ def game_hash
         }
         ]
     }
-  }
 end
