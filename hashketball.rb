4e11edafa077c1d85ba069ff087aea3e06a1b183
Jasper Reed <jmixon.reed@gmail.com>
2020-07-21 02:10:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,9 +70,9 @@ def game_hash
       :players = [
         {
           :player_name => "Jeff Adrien",
-          :numbner =>,
-          :shoe =>,
-          :points =>,
+          :numbner => 4,
+          :shoe => 18,
+          :points => 10,
           :rebounds =>,
           :assists =>,
           :steals =>,
