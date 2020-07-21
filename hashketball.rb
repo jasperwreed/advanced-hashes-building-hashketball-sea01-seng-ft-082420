a8aa41560ac505f24a4f65c9833aeb9436ea0798
Jasper Reed <jmixon.reed@gmail.com>
2020-07-21 02:10:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,7 +113,7 @@ def game_hash
           :slam_dunks =>,
         },
         {
-          :player_name => "",
+          :player_name => "Kemba Walker",
           :numbner =>,
           :shoe =>,
           :points =>,
